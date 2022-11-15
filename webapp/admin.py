1382b62b890a1816544628c6a1e70a333ac7cedf
Marat <marat.mashanlo@gmail.com>
2022-11-15 15:46:48 +0600

    1 etap

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from webapp.models import Product
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content', 'status', 'balance', 'price']
+    list_filter = ['status']
+    search_fields = ['title']
+    exclude = []
+
+
+admin.site.register(Product, ProductAdmin)
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+STATUS_CHOICES = [('other', 'Raznoe'), ('fruits', 'Fruits'), ('vegs', 'Vegs'), ('bakery', 'Bakery'), ('milk', 'Milk')]
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name='Naimenovanie tovara')
+    content = models.TextField(max_length=2000, null=False, blank=False, verbose_name='Opisanie tovara')
+    status = models.CharField(max_length=40, choices=STATUS_CHOICES, default='other', verbose_name='Status')
+    balance = models.IntegerField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+def __str__(self):
+    return f'{self.id}. {self.title}'
